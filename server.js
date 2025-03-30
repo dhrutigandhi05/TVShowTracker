bf899ced18451ca06246b3df6f2dad3430939529
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-29 22:35:23 -0400

    adding routes

--- server.js
+++ server.js
@@ -14,3 +14,27 @@ app.set('views', './views')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')))
+
+let currrentUser = null
+
+// routes setup
+app.get('/', (request, response) => {
+    response.redirect('/login')
+})
+
+app.get('/login', (request, response) => {
+    response.render('login')
+})
+
+app.post('/login', (request, response) => {
+    const {username, password} = request.body
+
+    db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
+        if (user) {
+            currrentUser = row
+            return response.redirect('/dashboard')
+        }
+
+        response.render('login', {error: 'Invalid username or password'})
+    })
+})
