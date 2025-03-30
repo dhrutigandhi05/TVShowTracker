de8a46598b07af7d49703469355053a2d09bc6e5
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-03-29 22:08:46 -0400

    config

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-const express = require('express');
-const path = require('path');
-// const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
+const sqlite3 = require('sqlite3').verbose()
+const {engine} = require('express-handlebars')
+const db = new sqlite3.Database('./tvshows.db')
+
+// handlebars setup
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+// middleware setup
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
